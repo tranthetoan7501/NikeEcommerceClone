0f3589cbbcf9d81fa6ce0d9797cf55592377933f
huynhnhutanhduy <duy.hnad@gmail.com>
2024-11-07 22:22:39 +0700

    feat: [Header] - Update UI when hover

--- app/components/Header/index.tsx
+++ app/components/Header/index.tsx
@@ -6,14 +6,14 @@ export function Header() {
   return (
     <div className="flex justify-center">
       <div className="w-full">
-        <Button className="w-full rounded-none bg-black hover:bg-black">
+        <Button className="w-full rounded-none bg-black hover:bg-black group">
           <Link
             href="https://oeforgood.com/blogs/news/oe-enfile-ses-bottes-et-devient-vigneron"
-            className="tracking-[.0625rem] text-[.8125rem] font-bold"
+            className="tracking-[.0625rem] text-[.8125rem] font-bold flex items-center justify-center gap-3"
           >
             On vient d'acheter notre 1er Domaine Oé dans le sud de la France ! 🥳
+            <ArrowRightIcon className="transition-transform duration-300 ease-in-out transform group-hover:scale-x-125 origin-left" />
           </Link>
-          <ArrowRightIcon />
         </Button>
       </div>
       <div></div>
