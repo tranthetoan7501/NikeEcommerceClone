b433f988c7f5ca60e5ede7ed70fbd0381d0fa9a1
huynhnhutanhduy <duy.hnad@gmail.com>
2024-12-03 22:14:19 +0700

    feat: [Header] - Update UI

--- app/components/Header/index.tsx
+++ app/components/Header/index.tsx
@@ -1,21 +1,25 @@
-import { Button } from "@/components/ui/button";
 import Navigation from "./Navigation";
 import { NikeIcon } from "@/app/icon";
 import UserToolsSection from "./UserToolsSection";
+import Link from "next/link";
 
 interface Props {}
 
 const Header = (props: Props) => {
   return (
-    <div className="w-full px-12">
+    <div className="w-full px-12 relative">
       <div className="flex item-center justify-between h-[3.75rem]">
-        <div className="h-full flex items-center justify-center">
-          <Button variant="outline" size="icon">
-            <NikeIcon />
-          </Button>
+        <div className="h-full flex-center">
+          <Link href="#">
+            <NikeIcon width={80} height={80} />
+          </Link>
         </div>
-        <Navigation />
-        <UserToolsSection />
+        <div className="flex-center">
+          <Navigation />
+        </div>
+        <div className="flex-center">
+          <UserToolsSection />
+        </div>
       </div>
     </div>
   );
